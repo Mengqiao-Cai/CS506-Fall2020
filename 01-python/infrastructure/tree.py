6e57ca39e1a706fb1c948cbe7ee2a86c88c9e5d7
Mengqiao <mengqiao@bu.edu>
2020-09-14 13:43:30 -0400

    implemented draw_tree function

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,13 @@
-def draw_tree():
-    print("tree not found")
+def draw_tree(n):
+    #draw n trees in a row
+    print("    /\    " * n)
+    print("   /  \   " * n)
+    print("  /    \  " * n)
+    print(" /------\ " * n)
+    print("    ||    " * n)
+    print("    ||    " * n)
+    print("    ||    " * n)
     return
+
+#test
+#draw_tree(4)
